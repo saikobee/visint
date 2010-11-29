0d5a8047e762e986412921cd11672a5342d524cc
Brian Mock <mock.brian@gmail.com>
2010-11-28 16:23:50 -0800

    Incorrect return type in interface

--- Func.java
+++ Func.java
@@ -3,5 +3,5 @@ public interface Func {
     public float valueAt(float x, float z);
 
     /** Returns the normal at a given (x, z). */
-    public float normalAt(float x, float z);
+    public float[] normalAt(float x, float z);
 }
