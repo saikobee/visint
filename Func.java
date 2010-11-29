404cdce15dfe6bd3ccef284339d3c51007f4b63c
Brian Mock <mock.brian@gmail.com>
2010-11-29 13:30:15 -0800

    Changed Func from interface to abstract class

--- Func.java
+++ Func.java
@@ -1,7 +1,20 @@
-public interface Func {
+public abstract class Func {
+    protected static final float PI     = (float) Math.PI;
+    protected static final float TWO_PI = 2f * PI;
+
     /** Returns the y-value at a given (x, z). */
-    public float valueAt(float x, float z);
+    public abstract float valueAt(float x, float z);
+
+    /** Returns the partial derivative with respect to x. */
+    public abstract float xPartial(float x);
+
+    /** Returns the partial derivative with respect to z. */
+    public abstract float zPartial(float z);
 
     /** Returns the normal at a given (x, z). */
-    public float[] normalAt(float x, float z);
+    public float[] normalAt(float x, float z) {
+        return new float[] {
+            xPartial(x), 0, zPartial(z)
+        };
+    }
 }
