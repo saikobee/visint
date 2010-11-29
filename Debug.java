028187c2ecb1285c1b7479b2125eb4fd4744762e
Brian Mock <mock.brian@gmail.com>
2010-11-28 16:28:28 -0800

    Fixed more non-staticness

--- Debug.java
+++ Debug.java
@@ -1,5 +1,5 @@
 public class Debug {
-    public boolean debug = true;
+    public static boolean debug = true;
 
     public static void println(String s) { if (debug) System.err.println(s); }
     public static void print  (String s) { if (debug) System.err.print  (s); }
