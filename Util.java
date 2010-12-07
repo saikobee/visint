feb591be68b391f18c67ecd17493c7368e872c45
Brian Mock <mock.brian@gmail.com>
2010-12-06 19:00:32 -0800

    Added some vector functions to Util

--- Util.java
+++ Util.java
@@ -15,4 +15,47 @@ public class Util {
             throw new RuntimeException(message);
         }
     }
+
+    static float[]
+    cross(float[] a, float[] b) {
+        // 23-32, 31-13, 12-21
+        return new float[] {
+            a[1]*b[2] - a[2]*b[1],
+            a[2]*b[0] - a[0]*b[2],
+            a[0]*b[1] - a[1]*b[0]
+        };
+    }
+
+    static float
+    magnitude(float[] v) {
+        float x = v[0];
+        float y = v[1];
+        float z = v[2];
+
+        return Math.sqrt(x*x + y*y + z*z);
+    }
+
+    static float[]
+    scale(float k, float[] v) {
+        return new float[] {
+            k*v[0],
+            k*v[1],
+            k*v[2]
+        };
+    }
+
+    static float[]
+    normalize(float[] v) {
+        float m = magnitude(v);
+        return scale(1f/m, v);
+    }
+
+    static float[]
+    displacementVector(float[] p1, float[] p2) {
+        return new float[] {
+            p1[0] - p2[0],
+            p1[1] - p2[1],
+            p1[2] - p2[2]
+        };
+    }
 }
