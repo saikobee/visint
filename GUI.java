b72e2812b3a1c4d3d8492f66e7af8d47b69a49b0
Brian Mock <mock.brian@gmail.com>
2010-12-20 23:08:46 -0800

    Added GPL licensing info

--- GUI.java
+++ GUI.java
@@ -1,3 +1,21 @@
+/* Copyright 2010 Brian Mock
+ *
+ *  This file is part of visint.
+ *
+ *  visint is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  visint is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with visint.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.awt.*;
 import java.awt.event.*;
 
