a9bfdabdd5d7cbd9e3c85cb21339206cbd34ccf4
Brian Mock <mock.brian@gmail.com>
2010-12-22 14:07:59 -0800

    Updated to include Linux launcher script

--- zip.sh
+++ zip.sh
@@ -1,9 +1,25 @@
-#!/bin/sh
-zip -r              \
-    "visint-1.1.zip" \
-    COPYING.txt       \
-    README.txt         \
-    *.dll               \
-    *.so                 \
-    lib/                  \
-    "visint-1.1.jar"
+#!/bin/bash
+
+text=(*.txt)
+libs=(visint *.dll *.so lib/)
+jar="visint-1.1.jar"
+
+stuff=("${text[@]}" "${libs[@]}" "$jar")
+
+output="visint-1.1"
+
+# Windows
+echo "Packaging for Windows"
+zip -r "$output.zip" "${stuff[@]}"
+
+echo
+
+# Linux
+echo "Packaging for Linux"
+if [ -e "$output" ]; then
+    rm -r "$output"
+fi
+mkdir "$output"
+cp -r "${stuff[@]}" "$output"
+tar -czvf "$output.tar.gz" "$output"
+rm -r "$output"
